42583970f05459d557bd0cfe71f000024185573b
HuuDiep1102 <56430293+HuuDiep1102@users.noreply.github.com>
2023-07-28 11:35:59 +0700

    Update ImagesPdfModule.java change create file to base64

--- android/src/main/java/com/imagespdf/ImagesPdfModule.java
+++ android/src/main/java/com/imagespdf/ImagesPdfModule.java
@@ -6,12 +6,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.pdf.PdfDocument;
 import android.graphics.Color;
+import android.util.Base64;
 
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.documentfile.provider.DocumentFile;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -21,176 +21,138 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 @ReactModule(name = ImagesPdfModule.NAME)
 public class ImagesPdfModule extends ReactContextBaseJavaModule {
-  public static final String NAME = "ImagesPdf";
+    public static final String NAME = "ImagesPdf";
 
-  public ImagesPdfModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    public ImagesPdfModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  @NonNull
-  public String getName() {
-    return NAME;
-  }
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
 
-  @ReactMethod
-  public void createPdf(ReadableMap options, Promise promise) {
-    try {
-      ReadableArray imagePaths = options.getArray("imagePaths");
-      String outputDirectory = options.getString("outputDirectory");
-      String outputFilename = options.getString("outputFilename");
+    @ReactMethod
+    public void createPdf(ReadableMap options, Promise promise) {
+        try {
+            ReadableArray imagePaths = options.getArray("imagePaths");
+            String outputFilename = options.getString("outputFilename");
 
-      if (imagePaths.size() == 0) {
-        throw new Exception("imagePaths is empty.");
-      }
+            if (imagePaths.size() == 0) {
+                throw new Exception("imagePaths is empty.");
+            }
 
-      PdfDocument pdfDocument = new PdfDocument();
+            PdfDocument pdfDocument = new PdfDocument();
 
-      try {
-        for (int i = 0; i < imagePaths.size(); ++i) {
-          String imagePath = imagePaths.getString(i);
-          Bitmap bitmap = getBitmapFromPathOrUri(imagePath);
+            try {
+                for (int i = 0; i < imagePaths.size(); ++i) {
+                    String imagePath = imagePaths.getString(i);
+                    Bitmap bitmap = getBitmapFromPathOrUri(imagePath);
 
-          if (bitmap == null) {
-            throw new Exception(imagePath + " cannot be decoded into a bitmap.");
-          }
+                    if (bitmap == null) {
+                        throw new Exception(imagePath + " cannot be decoded into a bitmap.");
+                    }
 
-          int pageWidth = 596;
-          int pageHeight = 842;
+                    int pageWidth = 596;
+                    int pageHeight = 842;
 
-          PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo
-            .Builder(pageWidth, pageHeight, i + 1)
-            .create();
+                    PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo
+                            .Builder(pageWidth, pageHeight, i + 1)
+                            .create();
 
-          PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+                    PdfDocument.Page page = pdfDocument.startPage(pageInfo);
 
-          Canvas canvas = page.getCanvas();
-          canvas.drawColor(Color.WHITE);
+                    Canvas canvas = page.getCanvas();
+                    canvas.drawColor(Color.WHITE);
 
-          float centerX = (pageWidth - bitmap.getWidth()) / 2;
-          float centerY = (pageHeight - bitmap.getHeight()) / 2;
+                    float centerX = (pageWidth - bitmap.getWidth()) / 2;
+                    float centerY = (pageHeight - bitmap.getHeight()) / 2;
 
-          canvas.drawBitmap(bitmap, centerX, centerY, null);
+                    canvas.drawBitmap(bitmap, centerX, centerY, null);
 
-          pdfDocument.finishPage(page);
+                    pdfDocument.finishPage(page);
+                }
+            } catch (Exception e) {
+                Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
+                promise.reject("PDF_PAGE_CREATE_ERROR", e.getLocalizedMessage(), e);
+                pdfDocument.close();
+                return;
+            }
+
+            String base64String = null;
+            try {
+                base64String = writePdfDocument(pdfDocument);
+            } catch (Exception e) {
+                Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
+                promise.reject("PDF_WRITE_ERROR", e.getLocalizedMessage(), e);
+                pdfDocument.close();
+                return;
+            }
+
+            pdfDocument.close();
+
+            promise.resolve(base64String);
+
+        } catch (Exception e) {
+            Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
+            promise.reject("PDF_CREATE_ERROR", e.getLocalizedMessage(), e);
         }
-      } catch (Exception e) {
-        Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
-        promise.reject("PDF_PAGE_CREATE_ERROR", e.getLocalizedMessage(), e);
-        pdfDocument.close();
-        return;
-      }
-
-      Uri outputUri = null;
-
-      try {
-        outputUri = writePdfDocument(pdfDocument, outputDirectory, outputFilename);
-      } catch (Exception e) {
-        Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
-        promise.reject("PDF_WRITE_ERROR", e.getLocalizedMessage(), e);
-        pdfDocument.close();
-        return;
-      }
-
-      pdfDocument.close();
-
-      promise.resolve(outputUri.getPath());
-    } catch (Exception e) {
-      Log.e("ImagesPdfModule", e.getLocalizedMessage(), e);
-
-      promise.reject("PDF_CREATE_ERROR", e.getLocalizedMessage(), e);
-    }
-  }
-
-  @ReactMethod
-  public void getDocumentsDirectory(Promise promise) {
-    String docsDir = getReactApplicationContext().getExternalFilesDir(null).getAbsolutePath();
-    promise.resolve(docsDir);
-  }
-
-  public Uri writePdfDocument(PdfDocument pdfDocument, String outputDirectory, String outputFilename) throws IOException {
-    OutputStream outputStream = null;
-    Uri outputUri = null;
-
-    try {
-      Uri uri = Uri.parse(outputDirectory);
-      String mimeTypePdf = "application/pdf";
-
-      String scheme = uri.getScheme();
-
-      if (scheme != null && scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-        // TODO: Fix warnings: Method invocation may produce 'NullPointerException'.
-
-        DocumentFile dirFile = DocumentFile
-          .fromTreeUri(getReactApplicationContext().getApplicationContext(), uri);
-
-        DocumentFile pdfFile = dirFile
-          .createFile(mimeTypePdf, outputFilename);
-
-        outputUri = pdfFile.getUri();
-
-        outputStream = getReactApplicationContext()
-          .getContentResolver()
-          .openOutputStream(outputUri);
-      } else if (scheme == null || scheme.equals(ContentResolver.SCHEME_FILE)) {
-        outputUri = uri.buildUpon()
-          .appendPath(outputFilename)
-          .build();
-
-        outputStream = new FileOutputStream(outputUri.getPath());
-      } else {
-        throw new UnsupportedOperationException("Unsupported scheme: " + uri.getScheme());
-      }
-
-      pdfDocument.writeTo(outputStream);
-    } finally {
-      if (outputStream != null) {
-        outputStream.close();
-      }
     }
 
-    return outputUri;
-  }
+    public String writePdfDocument(PdfDocument pdfDocument) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-  public Bitmap getBitmapFromPathOrUri(String pathOrUri) throws IOException {
-    Bitmap bitmap = null;
-    InputStream inputStream = null;
+        try {
+            pdfDocument.writeTo(outputStream);
+        } finally {
+            outputStream.close();
+        }
 
-    try {
-      Uri uri = Uri.parse(pathOrUri);
+        byte[] pdfBytes = outputStream.toByteArray();
+        String base64String = Base64.encodeToString(pdfBytes, Base64.DEFAULT);
 
-      String scheme = uri.getScheme();
-
-      if (scheme != null && scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-        ContentResolver contentResolver = getReactApplicationContext()
-          .getContentResolver();
-
-        inputStream = contentResolver
-          .openInputStream(uri);
-      } else if (scheme == null || scheme.equals(ContentResolver.SCHEME_FILE)) {
-        inputStream = new FileInputStream(uri.getPath());
-      } else {
-        throw new UnsupportedOperationException("Unsupported scheme: " + uri.getScheme());
-      }
-
-      if (inputStream != null) {
-        bitmap = BitmapFactory
-          .decodeStream(inputStream);
-      }
-    } finally {
-      if (inputStream != null) {
-        inputStream.close();
-      }
+        return base64String;
     }
 
-    return bitmap;
-  }
+    public Bitmap getBitmapFromPathOrUri(String pathOrUri) throws IOException {
+        Bitmap bitmap = null;
+        InputStream inputStream = null;
+
+        try {
+            Uri uri = Uri.parse(pathOrUri);
+
+            String scheme = uri.getScheme();
+
+            if (scheme != null && scheme.equals(ContentResolver.SCHEME_CONTENT)) {
+                ContentResolver contentResolver = getReactApplicationContext()
+                        .getContentResolver();
+
+                inputStream = contentResolver
+                        .openInputStream(uri);
+            } else if (scheme == null || scheme.equals(ContentResolver.SCHEME_FILE)) {
+                inputStream = new FileInputStream(uri.getPath());
+            } else {
+                throw new UnsupportedOperationException("Unsupported scheme: " + uri.getScheme());
+            }
+
+            if (inputStream != null) {
+                bitmap = BitmapFactory
+                        .decodeStream(inputStream);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return bitmap;
+    }
 }
